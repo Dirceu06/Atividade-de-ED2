4b95292f3336e6f9f91cd7b81ec755e9326722b2
EduardoHaagP <163572315+EduardoHaagP@users.noreply.github.com>
2025-09-02 21:38:52 -0300

    insertion e merge sort

--- ordenadores.py
+++ ordenadores.py
@@ -21,18 +21,76 @@ def bubbleSort(lista):
     timer = fim-inicio
     return lista, comparacoes, timer
 
-def selectionSort(vet, n):
+def selectionSort(lista, n):
     inicio = time.time() 
     comparacoes = 0
     for i in range(0,n-1):
         menor = i
         for j in range(i, n):
             comparacoes += 1
-            if vet[menor] > vet[j]:
+            if lista[menor] > lista[j]:
                 menor = j
         if menor != i:
-            vet[menor],vet[i]=vet[i],vet[menor]
+            lista[menor],lista[i]=lista[i],lista[menor]
         
     fim = time.time()
     timer = fim-inicio
-    return vet, comparacoes, timer
+    return lista, comparacoes, timer
+
+def insertionSort(lista):
+    inicio = time.time()
+    comparacoes = 0
+    for i in range(1,len(lista)):
+        k = i - 1
+        aux = lista[i]
+        while k >= 0 and aux < lista[k]:
+            comparacoes += 1
+            lista[k+1] = lista[k]
+            k -= 1
+        lista[k + 1] = aux
+    fim = time.time()
+    timer = fim-inicio
+    return lista, comparacoes, timer
+
+
+desordem = [-66 , -28, 41, -87, 51, -17, -92, -22, 18, 9]
+insertionSort(desordem)
+print(desordem)
+
+
+def mergeSort(lista,inicio, fim):
+    inicio = time.time()
+    global comparacoesMergeSort
+    if inicio < fim:
+        meio = (inicio+fim)//2
+        mergeSort(lista,inicio, meio)
+        mergeSort(lista, meio+1, fim)
+        
+        merge(lista, inicio,meio, fim)
+    fim = time.time()
+    timer = fim-inicio
+    return lista, comparacoesMergeSort, timer
+
+
+
+def merge(lista, inicio,meio, fim):
+    global comparacoesMergeSort
+    aux = []
+    p1 = inicio
+    p2 = meio + 1
+    
+    while p1 <= meio and p2 <= fim:
+        comparacoesMergeSort += 1
+        if lista[p1] < lista[p2]:
+            aux.append(lista[p1])
+            p1 += 1
+        else:
+            aux.append(lista[p2])
+            p2 += 1
+
+    aux.extend(lista[p1:meio+1])
+    aux.extend(lista[p2:fim+1])
+        
+    for i in range(len(aux)):
+        lista[inicio + i] = aux[i]
+
